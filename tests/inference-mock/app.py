234d57428a62108c204e38d32ea923eaa1652978
Emilio Garcia <i.am.emilio@gmail.com>
2025-05-20 14:21:03 -0400

    fix: handle issues raised in review for inference-mock
    Signed-off-by: Emilio Garcia <i.am.emilio@gmail.com>

--- tests/inference-mock/app.py
+++ tests/inference-mock/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, Response, jsonify
-import logging
-
+from werkzeug import exceptions
 from qna.qna import MockFactGenerator
+
+import logging
 
 # Globals
 app = Flask(__name__)
@@ -9,7 +10,7 @@ port_number = 11434
 app.logger.setLevel(logging.DEBUG)
 fact_generator = MockFactGenerator(app.logger)
 
-def completion_response(content:str, model:str="gpt-3.5") -> dict:
+def completion_response(content: str, model: str="gpt-3.5") -> dict:
     response = {
         "id": "chatcmpl-2nYZXNHxx1PeK1u8xXcE1Fqr1U6Ve",
         "object": "chat.completion",
@@ -38,13 +39,14 @@ def completion_response(content:str, model:str="gpt-3.5") -> dict:
 @app.route('/v1/completions', methods=['POST'])
 def mock_generate():
     if not request.json or 'prompt' not in request.json:
-        app.logger.debug("prompt is empty or None")
-        return Response("bad request", 500)
+        issue = "prompt is empty or None"
+        app.logger.debug(issue)
+        raise exceptions.BadRequest(issue)
 
     prompt_debug_str = request.json['prompt'][:90] + "..."
     app.logger.debug(f"{request.method} {request.url} {request.json['model']} {prompt_debug_str}")
     if request.json == None:
-        return Response("Bad Request: request json empty", status=404)
+        raise exceptions.BadRequest("Bad Request: request json empty")
     
     prompt = request.json['prompt']
     
@@ -54,7 +56,6 @@ def mock_generate():
         completion = ""
 
     response = jsonify(completion_response(completion, request.json['model']))
-
     app.logger.debug(f"response: {response}")
     return response
     
--- tests/inference-mock/qna/qna.py
+++ tests/inference-mock/qna/qna.py
@@ -9,25 +9,15 @@ You can use the empty response template below when adding a new response to this
 Since the chunks are randomly selected, there is not a good way to associate the responses to the chunks at this time. That should
 not be necessary for basic testing anyway.
 '''
-responses = [
-   {
+response = {
         "fact_single":"What are some common ways to assign rewards to partial answers?",
         "fact_single_answer":"There are three: prod , which takes the product of rewards across all steps; min , which selects the minimum reward over all steps; and last, which uses the reward from the final step.",
         "reasoning":"What is the best method for rewarding models?",
         "reasoning_answer":"That depends on whether the training data is prepared using MC rollout, human annotation, or model annotation.",
         "summary":"How does QWEN implement model reward?",
         "summary_answer":"Qwen computes the aggregate reward based on the entire partial reward trajectory. I also uses a method that feeds the performance reference model with partial answers, then only considering the final reward token.",
-    },
-   ]
-
-empty_response = {
-    "fact_single":"",
-    "fact_single_answer":"",
-    "reasoning":"",
-    "reasoning_answer":"",
-    "summary":"",
-    "summary_answer":"",
-}
+    }
+   
 
 fact_prompt_match = "I need you to generate three questions that must be answered only with information contained in this passage, and nothing else."
 
@@ -37,8 +27,6 @@ class MockFactGenerator:
     """
     def __init__(self, logger:logging.Logger):
         self.logger = logger
-        self._lock = threading.Lock()
-        self._index = 0
 
     def generatePair(self, prompt:str):
         ''' 
@@ -49,7 +37,4 @@ class MockFactGenerator:
             self.logger.debug("prompt is not for Q&A gen")
             return None
         
-        with self._lock:
-            response = responses[self._index]
-            self._index = (self._index + 1) % len(responses)
-            return json.dumps(response)
+        return json.dumps(response)
