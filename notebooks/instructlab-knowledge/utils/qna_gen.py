5df1db2932a16c2a26eb95ec4a58388552c50483
Ali Maredia <amaredia@redhat.com>
2025-06-05 10:34:26 -0400

    SQUASH-ME: Cleaner commit to squash into
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- notebooks/instructlab-knowledge/utils/qna_gen.py
+++ notebooks/instructlab-knowledge/utils/qna_gen.py
@@ -1,60 +1,17 @@
-# CELL 0
-from pathlib import Path
-
-from docling_sdg.qa.generate import Generator
-from docling_sdg.qa.base import GenerateOptions, LlmProvider
-from pydantic import SecretStr
 import json
 import yaml
 import random
+
+from pathlib import Path
+from pydantic import SecretStr
 from textwrap import wrap
 
 from docling_core.transforms.chunker.hierarchical_chunker import DocChunk, DocMeta
 from docling_sdg.qa.utils import get_qa_chunks
+from docling_sdg.qa.generate import Generator
+from docling_sdg.qa.base import GenerateOptions, LlmProvider
 
-API_KEY = "foo"
-API_URL = "bar"
-MODEL_ID = "baz"
-
-generate_options = GenerateOptions(project_id="project_id")
-generate_options.provider = LlmProvider.OPENAI_LIKE
-generate_options.api_key = SecretStr(API_KEY)
-generate_options.url = API_URL
-generate_options.model_id = MODEL_ID
-
-NUM_CHUNKS_PER_CONTRIBUTION_TO_SELECT_FOR_AUTHORING = 5
-
-WORKSPACE_NAME = "default"
-
-WORKSPACE_ROOT = Path("workspaces")
-WORKSPACE_ROOT.mkdir(exist_ok=True)
-
-WORKSPACE_DIR = WORKSPACE_ROOT / WORKSPACE_NAME
-WORKSPACE_DIR.mkdir(exist_ok=True)
-
-contribution_dirs = []
-# contribution_names = ["nfl"]  # ADD CONTRIBUTION NAMES HERE
-contribution_names = ["nfl", "finance"]
-
-contribution_domain = "sports"
-contribution_summary = "Official playing rules of the National Football League 2022"
-
-# contribution1 = {"path": contribution_path, "prefix": contribution_prefix, "domain": contribution_domain, "summary": contribution_summary}
-#contribution1 = {"path": contribution_path, "domain": contribution_domain, "summary": contribution_summary}
-
-# contribution_path2 = Path(SOURCE_DOCUMENT_DIR / "finance")
-# contribution_prefix2 = "finance"
-# contribution_domain2 = "banking"
-# contribution_summary2 = "Account information for a specific bank"
-
-for name in contribution_names:
-    contribution_dir = WORKSPACE_DIR / name
-    contribution_dirs.append(contribution_dir)
-
-print(f"Contribution names are: {contribution_names}")
-print(f"Contribution dir are: {contribution_dirs}")
-
-filters = [
+chunk_filter = [
     lambda chunk: len(str(chunk.text)) > 500
 ]
 
@@ -75,12 +32,29 @@ class IndentedDumper(yaml.Dumper):
     def increase_indent(self, flow=False, indentless=False):
         return super(IndentedDumper, self).increase_indent(flow, False)
 
-dataset = {}
-for contribution_name, contribution_dir in zip(contribution_names, contribution_dirs):
+def generate_seed_examples(contribution_name: str, contribution_dir: str, contribution_metadata: dict, num_seed_examples: int, api_key: str, api_url: str, model_id: str) -> Path:
+    """
+    Creates a seed dataset from a path
+    Args:
+        contribution_name (str):        Name of the contribution
+        contribution_dir (str):         Path to the artifacts for the contribution. contribution_dir should contain
+                                        a chunks/chunks.jsonl file
+        contribution_metadata (dict):   Dictionary with the domain and summary of the contribution
+        num_seed_examples (str):        Number of seed examples user wishes to generate for the contribution
+        api_key (str):                  API key for the model used to generate questions and answers from contexts
+        api_url (str):                  Endpoint for the model used to generate questions and answers from contexts
+        model_id (str):                 Name of the model used to generate questions and answers from contexts
+    Returns:
+        qna_output_path (pathlib.Path): Path to the generated seed example file
+    """
+    dataset = {}
     dataset[contribution_name] = {}
     dataset[contribution_name]["chunks"] = []
 
     chunks_jsonl_path = contribution_dir / "chunks" / "chunks.jsonl"
+    if not chunks_jsonl_path.exists():
+        raise ValueError(f"chunks.jsonl does not exist but should at {chunks_jsonl_path}")
+
     docs = []
 
     with open(chunks_jsonl_path, 'r') as file:  # khaled was here
@@ -101,25 +75,27 @@ for contribution_name, contribution_dir in zip(contribution_names, contribution_
                 docs.append(doc)
 
     for doc in docs:
-        print(f"Chunking and filtering document {doc['file']}")
+        print(f"Filtering smaller chunks out of document {doc['file']}")
         
-        qa_chunks = get_qa_chunks(doc["file"], doc["chunk_objs"], filters)
+        qa_chunks = get_qa_chunks(doc["file"], doc["chunk_objs"], chunk_filter)
         dataset[contribution_name]["chunks"].extend(list(qa_chunks))
-
-    generate_options.generated_file = contribution_dir / "authoring" / f"qagen-{contribution_name}.json"
-    gen = Generator(generate_options=generate_options)
 
 
     l = dataset[contribution_name]["chunks"]
-    print(f"ALI DEBUG: {len(l)}")
-    selected_chunks = random.sample(l, NUM_CHUNKS_PER_CONTRIBUTION_TO_SELECT_FOR_AUTHORING)
+    selected_chunks = random.sample(l, num_seed_examples)
 
-    print(f"ALI DEBUG: {selected_chunks}")
+    generate_options = GenerateOptions(project_id="project_id")
+    generate_options.provider = LlmProvider.OPENAI_LIKE
+    generate_options.api_key = SecretStr(api_key)
+    generate_options.url = api_url
+    generate_options.model_id = model_id
+    generate_options.generated_file = contribution_dir / "authoring" / f"qagen-{contribution_name}.json"
+    gen = Generator(generate_options=generate_options)
 
     Path.unlink(generate_options.generated_file, missing_ok=True)
     results = gen.generate_from_chunks(selected_chunks) # automatically saves to file
 
-    print(f"{contribution_name}: {results.status}")
+    print(f"Status for Q&A generation for {contribution_name} is: {results.status}")
 
     qnas = {}
     chunk_id_to_text = {}
@@ -132,9 +108,6 @@ for contribution_name, contribution_dir in zip(contribution_names, contribution_
             if chunk_id not in qnas:
                 qnas[chunk_id] = []
             qnas[chunk_id].append({'question': entry['question'], 'answer': entry['answer']})
-
-    print(f"Generated QA pairs for {len(qnas)} contexts")
-    print(list(qnas.values())[0])
 
     qna_output_path = contribution_dir / "authoring" / "qna.yaml"
     
@@ -150,11 +123,11 @@ for contribution_name, contribution_dir in zip(contribution_names, contribution_
             ]
         })
     
-    data['document_outline'] = contribution_summary
-    data['domain'] = contribution_domain
+    data['document_outline'] = contribution_metadata["summary"]
+    data['domain'] = contribution_metadata["domain"]
     
     Path.unlink(qna_output_path, missing_ok=True) # shouldn't be necessary but was. jupyter caching thing?
     with open(qna_output_path, 'w') as yaml_file:
         yaml.dump(data, yaml_file, Dumper=IndentedDumper, default_flow_style=False, sort_keys=False, width=80)
     
-    print(f"qna.yaml saved to: {qna_output_path}")
+    return qna_output_path
